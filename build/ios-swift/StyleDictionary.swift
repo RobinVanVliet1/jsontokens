38850944455f81a1774c9260cb8c20dfbf6cbc15
RobinVanVliet1 <RobinVanVliet1@users.noreply.github.com>
2022-03-16 11:01:47 +0000

    Tokens updated

---
+++ build/ios-swift/StyleDictionary.swift
@@ -0,0 +1,34 @@
+
+//
+// StyleDictionary.swift
+//
+
+// Do not edit directly
+// Generated on Wed, 16 Mar 2022 11:01:45 GMT
+
+
+import UIKit
+
+public class StyleDictionary {
+    public static let bordersDashedOutside = [object Object]
+    public static let bordersSingle = [object Object]
+    public static let bordersSingleStyle = [object Object]
+    public static let breakpointsLg = 1280
+    public static let breakpointsMd = 1024
+    public static let breakpointsSm = 768
+    public static let radiiMixed = [object Object]
+    public static let radiiSmoothing = [object Object]
+    public static let radius5 = [object Object]
+    public static let sizes40 = 40
+    public static let sizes60 = 60
+    public static let sizes80 = 80
+    public static let sizesInVariant120 = 120
+    public static let sizesInVariant60 = 60
+    public static let sizesInVariant90 = 90
+    public static let sizesPlainToken = 200
+    public static let sizesTokenInFrame = 200
+    public static let sizesTokenInGroup = 200
+    public static let spacing10 = [object Object]
+    public static let spacingMixed = [object Object]
+    public static let spacingTop = [object Object]
+}
